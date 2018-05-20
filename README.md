11a52d5ce8f8f91842f7eb24bde8c3a746656270
Mohamed Elgemaiey <m.elgemaiey@gmail.com>
2018-05-20 11:37:23 -0400

    TensorFlow logistic regression

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Play and learn machine and deep learning in python
 |Model|file|Training|Test|
 |-----|----|--------|----|
 |keras|`models/keras_mnist.h5`|0.9982|0.9848|
+|tf logistic regression|`tf_mnist_log_reg.py`||0.929|
---
+++ tf_mnist_log_reg.py
@@ -0,0 +1,32 @@
+# Logistic Regression
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+#print(mnist.train)
+
+# define the model
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+#train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer().minimize(cross_entropy)
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+epochs = 20
+batch_size = 100
+batches_per_epoch = 60000 // batch_size
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+for i in range(epochs):
+  for _ in range(batches_per_epoch):
+    batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+  print("Epoch:", i+1,  "  Accuracy:", sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
