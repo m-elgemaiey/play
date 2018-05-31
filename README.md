37c702fe7daa71f620cb2295044e7cdb20eacd3f
Mohamed Elgemaiey <m.elgemaiey@gmail.com>
2018-05-30 21:49:41 -0400

    Play with LSTM

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ Training and test accuracy for different models:
 
 |Model|file|Training|Test|
 |-----|----|--------|----|
-|keras neural network|`keras_mnist.py`|0.9982|0.9848|
-|tf logistic regression|`tf_mnist_log_reg.py`|0.9354|0.9262|
-|tf neural network|`tf_mnist_nn.py`|0.9991|0.9828|
-|tf convolution neural network|`tf_mnist_cnn.py`|0.9988|0.9903|
+|keras neural network|keras_mnist.py|0.9982|0.9848|
+|tf logistic regression|tf_mnist_log_reg.py|0.9354|0.9262|
+|tf neural network|tf_mnist_nn.py|0.9991|0.9828|
+|tf convolution|tf_mnist_cnn.py|0.9988|0.9903|
+|tf LSTM|tf_mnist_lstm.py|0.9974|0.9861|
---
+++ tf_mnist_lstm.py
@@ -0,0 +1,86 @@
+import tensorflow as tf
+from tensorflow.contrib import rnn
+import time
+
+#import mnist dataset
+from tensorflow.examples.tutorials.mnist import input_data
+mnist=input_data.read_data_sets("MNIST_data/",one_hot=True)
+
+#define constants
+#unrolled through 28 time steps
+time_steps = 28
+#hidden LSTM units
+num_units = 128
+#rows of 28 pixels
+n_input = 28
+#mnist is meant to be classified in 10 classes(0-9).
+n_classes = 10
+#size of batch
+batch_size = 100
+epochs = 30
+batches_per_epoch = mnist.train.num_examples // batch_size
+
+
+#weights and biases of appropriate shape to accomplish above task
+out_weights = tf.Variable(tf.random_normal([num_units,n_classes]))
+out_bias = tf.Variable(tf.random_normal([n_classes]))
+
+#defining placeholders
+#input image placeholder
+x = tf.placeholder("float",[None, time_steps, n_input])
+#input label placeholder
+y = tf.placeholder("float",[None, n_classes])
+
+#processing the input tensor from [batch_size,n_steps,n_input] to "time_steps" number of [batch_size,n_input] tensors
+input=tf.unstack(x, time_steps, 1)
+
+#defining the network
+lstm_layer=rnn.BasicLSTMCell(num_units,forget_bias=1)
+outputs,_=rnn.static_rnn(lstm_layer,input,dtype="float32")
+
+#converting last output of dimension [batch_size,num_units] to [batch_size,n_classes] by out_weight multiplication
+prediction = tf.matmul(outputs[-1], out_weights) + out_bias
+
+#loss_function
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction,labels=y))
+#optimization
+opt = tf.train.AdamOptimizer().minimize(loss)
+
+#model evaluation
+correct_prediction = tf.equal(tf.argmax(prediction,1),tf.argmax(y,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
+summary_writer = tf.summary.FileWriter('logs/tf_lstm_' + time.strftime('%Y%m%d_%H%M%S'))
+
+#initialize variables
+init = tf.global_variables_initializer()
+
+def run_epoch(batches_per_epoch, batch_src):
+    epoch_acc = 0
+    epoch_loss = 0
+    for _ in range(batches_per_epoch):
+        batch_xs, batch_ys = batch_src.next_batch(batch_size)
+        batch_xs = batch_xs.reshape((batch_size, time_steps, n_input))
+        _, lossVal, accVal = sess.run([opt, loss, accuracy], feed_dict={x: batch_xs, y: batch_ys})
+        epoch_loss += lossVal
+        epoch_acc += accVal
+    epoch_acc /= batches_per_epoch
+    epoch_loss /= batches_per_epoch
+
+    return epoch_acc, epoch_loss
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for i in range(epochs):
+        epoch_acc, epoch_loss = run_epoch(batches_per_epoch, mnist.train)
+        print("Epoch:", i+1,  "  Accuracy:", epoch_acc)
+        acc_summ = tf.Summary(value=[tf.Summary.Value(tag='acc', simple_value=epoch_acc)])
+        loss_summ = tf.Summary(value=[tf.Summary.Value(tag='loss', simple_value=epoch_loss)])
+        summary_writer.add_summary(loss_summ, i)
+        summary_writer.add_summary(acc_summ, i)
+
+    #calculating test accuracy
+    batches_per_epoch = mnist.test.num_examples // batch_size
+    epoch_acc, _ = run_epoch(batches_per_epoch, mnist.test)
+    print('Testing accuracy:', epoch_acc)
