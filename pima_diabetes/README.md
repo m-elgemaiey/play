7a512df551bdc5c8bd793656bf84c62da297cf57
Mohamed Elgemaiey <m.elgemaiey@gmail.com>
2018-12-23 14:12:31 -0500

    Trials

--- pima_diabetes/README.md
+++ pima_diabetes/README.md
@@ -2,3 +2,9 @@
 Predict the onset of diabetes based on diagnostic measures.
 
 Data source: https://www.kaggle.com/uciml/pima-indians-diabetes-database
+
+## Notes:
+1. Test accuracy between 0..72 and 0.78
+2. Training accuracy ~1
+3. Dropout layers didn't help
+4. Maybe we need more data
--- pima_diabetes/keras_pima_diabetes.py
+++ pima_diabetes/keras_pima_diabetes.py
@@ -6,36 +6,34 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from pandas_ml import ConfusionMatrix
 
-
+# read data
 csv = pd.read_csv('diabetes.csv')
 print(csv.head())
 X = csv.loc[:, 'Pregnancies':'Age']
 y = csv.loc[:,'Outcome'].as_matrix()
 y = y.reshape(y.shape[0], 1)
-#y = csv.values(['Outcome'])
-#print(y)
+# Normalize
 X = preprocessing.scale(X)
+# split
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
-
+# model
 model = Sequential()
-model.add(Dense(128, activation='relu', input_shape=(8,)))
-model.add(Dropout(0.2))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.2))
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu', input_shape=(8,)))
+#model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+#model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+#model.add(Dropout(0.1))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(X_train, y_train, epochs=10)
+model.fit(X_train, y_train, epochs=200)
 score = model.evaluate(X_test, y_test)
 # print score
 print('Test score:')
 for i in range(len(model.metrics_names)):
     print(model.metrics_names[i], score[i])
-
+# confusion matrix
 y_pred = model.predict(X_test)
 y_pred = np.where(y_pred > 0.5, 1, 0)
 y_test = y_test.reshape(y_test.shape[0])
