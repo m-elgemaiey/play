2073eaf0d31e892ebd91b43f41c928f5c2e9ad83
Mohamed Elgemaiey <m.elgemaiey@gmail.com>
2019-01-30 23:35:34 -0500

    Used ImageDataGenerator

--- kaggle_mnist/README.md
+++ kaggle_mnist/README.md
@@ -12,8 +12,9 @@ https://www.kaggle.com/c/digit-recognizer
 | 4 | 0.99414 | Same as 3 with no dropout layers and used acc = `0.99992` to stop the training |
 | 5 | 0.99300 | same as 4 but with using loss as the stopping criteria and we stopped at loss = `1.1055e-06`. Didn't improve test accuracy.|
 | 6 | 0.99071 | same as 5 but stopped at loss = `1.1921e-07`, used 160 epochs, reached the same loss a lot earlier. Didn't improve test accuracy.|
-| 9 | <b>0.99471</b> | 64 filters  (5, 5) --> batch normalization --> relu --> max pooling (2, 2) --> 128 filters  (5, 5) --> batch normalization --> relu --> max pooling (2, 2) --> 256 filters  (5, 5) --> batch normalization --> relu --> max pooling (2, 2) --> 512 filters  (5, 5) --> batch normalization --> relu --> max pooling (2, 2) --> Dense (512) --> Dropout(0.2) --> Dense (512) --> Dropout(0.2) --> softmax|
+| 9 | 0.99471 | 64 filters  (5, 5) --> batch normalization --> relu --> max pooling (2, 2) --> 128 filters  (5, 5) --> batch normalization --> relu --> max pooling (2, 2) --> 256 filters  (5, 5) --> batch normalization --> relu --> max pooling (2, 2) --> 512 filters  (5, 5) --> batch normalization --> relu --> max pooling (2, 2) --> Dense (512) --> Dropout(0.2) --> Dense (512) --> Dropout(0.2) --> softmax|
 | 10 | 0.99371 | Inspired by the Inception model, see [keras_inception.py](keras_incpetion.py) | 
+| 11 | <b>0.99600</b> | Same as 9 but used ImageDataGenerator to add generated images |
 
 ## Notes:
 1. When increased the number of epochs with no dropout from 60 to 160 the model achieved a higher accuracy (lower loss) but failed to improve the test accuracy
--- kaggle_mnist/keras_cnn.py
+++ kaggle_mnist/keras_cnn.py
@@ -3,13 +3,14 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten, BatchNormalization, Activation
 from keras.utils import to_categorical
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 
 # input
 print('Reading MNIST data from train.csv ...')
 csv = pd.read_csv('train.csv', encoding='iso-8859-1')
-X = csv.loc[:, 'pixel0':].as_matrix()
-y = csv.loc[:, 'label'].as_matrix()
+X = csv.loc[:, 'pixel0':].values
+y = csv.loc[:, 'label'].values
 num_classes = 10
 X = X.reshape(-1, 28, 28, 1).astype('float32') / 255
 y = to_categorical(y, num_classes)
@@ -43,12 +44,18 @@ model.summary()
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 lr_cb = ReduceLROnPlateau(monitor='loss', factor=0.1, patience=5, verbose=1)
 es_cb = EarlyStopping(monitor='acc', patience=10, restore_best_weights=True, verbose=1)
-model.fit(X, y, epochs=100, batch_size=128, callbacks=[lr_cb, es_cb])
-
+#model.fit(X, y, epochs=100, batch_size=128, callbacks=[lr_cb, es_cb])
+datagen = ImageDataGenerator(
+    rotation_range=10,  
+    width_shift_range=0.1, 
+    height_shift_range=0.1,
+    zoom_range=0.1,
+    shear_range=0.3) 
+model.fit_generator(datagen.flow(X, y, batch_size=128), epochs=100, steps_per_epoch=X.shape[0] // 128, callbacks=[lr_cb, es_cb])
 # load test data
 print('Reading MNIST data from test.csv ...')
 csv = pd.read_csv('test.csv', encoding='iso-8859-1')
-X = csv.loc[:, 'pixel0':].as_matrix()
+X = csv.loc[:, 'pixel0':].values
 X = X.reshape(-1, 28, 28, 1).astype('float32') / 255
 y = model.predict(X)
 y = np.argmax(y, 1)
